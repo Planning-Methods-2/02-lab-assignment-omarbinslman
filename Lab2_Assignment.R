4291f10c106e6569ce894bcd2ad0584048714dbe
omarbinslman <omar.albalawi@my.utsa.edu>
2025-02-06 21:18:17 -0600

    Assignment 2 Date: 06\02\2025 Time: 9:20 PM

--- Lab2_Assignment.R
+++ Lab2_Assignment.R
@@ -32,9 +32,78 @@
 # Investigate (on the internet) how to add a title,a subtitle and a caption to your last plot. Create a new plot with that and save it as 'githubusername_p_extra.pdf'
 
 
+#Q1 
+library(data.table)
+
+
+opportunities<-fread(file = "datasets/tract_covariates.csv",header = T)
+
+# what is the object class?
+
+class(opportunities)  #object class are "data.table" "data.frame"
+
+# how can I know the variable names?
+names(opportunities)  # we can know the names by using names() function
+
+# What is the unit of analysis? 
+#Ans: the unit of analysis is Census Tract as discriped in Neighborhood Characteristics by Census Tract also (A census tract in the United States has an 11-digit code) if we take czname = Winchester as an example we have 2 digits for state and 3 dgits for county tract 6 
+
+
+# Use the `summary` function to describe the data. What is the variable that provides more interest to you?
+
+summary(opportunities) #The variable ann_avg_job_growth_2004_2013 because it helps analyze economic trends and market performance. a higher rate shows economic growth while negative rate may show economic degrowth. 
+
+head(opportunities)
+
+library(dplyr)
 
 
 
+sa_opportunities <- opportunities %>% filter(czname == "San Antonio")
+
+top10_growth <- sa_opportunities[order(-sa_opportunities$ann_avg_job_growth_2004_2013), ][1:10, ]
+
+
+
+
+githubusername_p1 <- ggplot(data = top10_growth, aes(x = reorder(tract, ann_avg_job_growth_2004_2013),
+                                                     y = ann_avg_job_growth_2004_2013)) +
+  geom_bar(stat = "identity")
+
+print(githubusername_p1)
+
+ggsave("githubusername_p1.pdf", width = 4, height = 4, githubusername_p1)
+
+
+
+githubusername_p3 <- ggplot(data = sa_opportunities, aes(x = frac_coll_plus2010, y = hhinc_mean2000)) +
+  geom_point(alpha = 0.6, color = "blue") 
+
+print(githubusername_p3)
+ggsave("githubusername_p3.pdf", width = 4, height = 4, githubusername_p3)
+
+
+
+#The plot shows a positive relationship between education (frac_coll_plus2010) and income (hhinc_mean2000) areas with more college graduates tend to have higher household incomes.
+#what is the causality direction?
+#more education leads to better jobs and higher salaries
+
+
+githubusername_p_extra <- ggplot(data = sa_opportunities, aes(x = frac_coll_plus2010, y = hhinc_mean2000)) +
+  geom_point(alpha = 0.6, color = "blue") +
+  labs(
+    title = "Relationship Between Higher Education and Household Income",
+    subtitle = "Scatter plot for the Relationship Between Higher Education and Household Income",
+    caption = "Source: Census Bureau"
+  )  
+
+print(githubusername_p_extra)
+
+ggsave("githubusername_p_extra.pdf", width = 4, height = 4, githubusername_p_extra)
+
+
+
+#source https://youtu.be/N3vl7THgy0U?si=VH_r_soM3h32czCI 
 
 
 
--- Lab2_Script.R
+++ Lab2_Script.R
@@ -20,41 +20,41 @@
 ## ---- Part 1.1: Loading data from R pre-loaded packages ----
 
 data() # shows all preloaded data available in R in the datasets package
-help(package="datasets")
+help(package="datasets")  # it opens the documentation for package"datasets" in help section 
 
 #Let's us the Violent Crime Rates by US State data 
 
-help("USArrests")
+help("USArrests") #it shows the Violent Crime Rates by US State in help section 
 
 # Step 1. Load the data in you session by creating an object
 
 usa_arrests<-datasets::USArrests # this looks the object 'USAarrests' within '::' the package 'datasets'
 
-class(usa_arrests)
-names(usa_arrests)
-dim(usa_arrests)
-head(usa_arrests)
-tail(usa_arrests)
+class(usa_arrests) #it shows the class of "usa_arrets" which is "data.frame" 
+names(usa_arrests) #it shows the names of each column in usa_arrests 
+dim(usa_arrests) #it gives us the numbers of rows and columns in usa_arrests which is 50 rows and 4 columns  
+head(usa_arrests) #it shows us the first 6 rows with its data  
+tail(usa_arrests) #it shows us the last 6 rows with its data  
 
 ## ---- Part 1.2: Loading data from your computer directory ----
 # We will use the Building Permits data from the city of San Antonio open data portal
 # Source: https://data.sanantonio.gov/dataset/building-permits/resource/c21106f9-3ef5-4f3a-8604-f992b4db7512
 
-building_permits_sa<-read.csv(file = "datasets/accelaissuedpermitsextract.csv",header = T)
+building_permits_sa<-read.csv(file = "datasets/accelaissuedpermitsextract.csv",header = T)   #it calls the building_permits_sa file from the file location 
 
-names(building_permits_sa)
-View(building_permits_sa)
-class(building_permits_sa)
-dim(building_permits_sa)
-str(building_permits_sa)
-summary(building_permits_sa)
+names(building_permits_sa) #it gives us the names of the columns in building_permits_sa
+View(building_permits_sa) # it shows or opens the data in a window
+class(building_permits_sa) #it shows the class of building_permits_sa which is "data.frame" 
+dim(building_permits_sa) #it gives us the numbers of rows and columns in building_permits_sa which is 5232 rows and 16 columns 
+str(building_permits_sa) # I did not understand this one but when I googled it it says "it used to display the internal structure of an object"  
+summary(building_permits_sa) #it gives us a summary of each column in building_permits_sa  
 
 
 ## ---- Part 1.3: Loading data directly from the internet ----
 # We will use the Building Permits data from the city of San Antonio open data portal
 # Source: https://data.sanantonio.gov/dataset/building-permits/resource/c21106f9-3ef5-4f3a-8604-f992b4db7512
 
-building_permits_sa2 <- read.csv("https://data.sanantonio.gov/dataset/05012dcb-ba1b-4ade-b5f3-7403bc7f52eb/resource/c21106f9-3ef5-4f3a-8604-f992b4db7512/download/accelaissuedpermitsextract.csv",header = T) 
+building_permits_sa2 <- read.csv("https://data.sanantonio.gov/dataset/05012dcb-ba1b-4ade-b5f3-7403bc7f52eb/resource/c21106f9-3ef5-4f3a-8604-f992b4db7512/download/accelaissuedpermitsextract.csv",header = T) # Load the file from the internet or the written link  
 
 
 
@@ -62,58 +62,58 @@ building_permits_sa2 <- read.csv("https://data.sanantonio.gov/dataset/05012dcb-b
 ## ---- Part 1.4: Loading data using a package + API ----
 #install.packages("tidycensus")
 #install.packages("tigris")
-help(package="tidycensus")
-library(tidycensus)
-library(tigris)
+help(package="tidycensus") #it opens the help Documentation  in help section  
+library(tidycensus) #Load the library tidycensus 
+library(tigris) #Load the library tigris  
 
 
 #type ?census_api_key to get your Census API for full access.
-census_api_key("0d539976d5203a96fa55bbf4421110d4b3db3648", overwrite = TRUE)
+census_api_key("0d539976d5203a96fa55bbf4421110d4b3db3648", overwrite = TRUE)  #this is my census_api_key to get access for  
 
 
 age10 <- get_decennial(geography = "state", 
                        variables = "P013001", 
-                       year = 2010)
+                       year = 2010) #it retrieves median age for each state for the year 2010  
 
-head(age10)
+head(age10)  #it shows the first 6 rows with its data  
 
 
 bexar_medincome <- get_acs(geography = "tract", variables = "B19013_001",
-                           state = "TX", county = "Bexar", geometry = T)
+                           state = "TX", county = "Bexar", geometry = F) #Retrieve median household income for Bexar County with out geometry  
 
 
-View(bexar_medincome)
+View(bexar_medincome) #it opens the bexar_medincome in a window   
 
-class(bexar_medincome)
+class(bexar_medincome) #it shows the class of bexar_medincome which are "tbl_df"     "tbl"        "data.frame" 
 
 #---- Part 2: Visualizing the data ----
 #install.packages('ggplot2')
 
-library(ggplot2)
+library(ggplot2) #load ggplot2 library for Visualizing the data 
 
 
 
 ## ---- Part 2.1: Visualizing the 'usa_arrests' data ----
 
-ggplot()
+ggplot() # it create an empty map on plots window 
 
 #scatter plot - relationship between two continuous variables
 ggplot(data = usa_arrests,mapping = aes(x=Assault,y=Murder)) +
-  geom_point()
+  geom_point() # it create a scatter plot between x=Assault,y=Murder
 
 ggplot() +
-  geom_point(data = usa_arrests,mapping = aes(x=Assault,y=Murder))
+  geom_point(data = usa_arrests,mapping = aes(x=Assault,y=Murder)) #same as line 101 but a new method 
 
 
 #bar plot - compare levels across observations
-usa_arrests$state<-rownames(usa_arrests)
+usa_arrests$state<-rownames(usa_arrests) # it creates a new column called state in  usa_arrests 
 
 ggplot(data = usa_arrests,aes(x=state,y=Murder))+
-  geom_bar(stat = 'identity')
+  geom_bar(stat = 'identity') # It creates a bar graph between x=state,y=Murder 
 
 ggplot(data = usa_arrests,aes(x=reorder(state,Murder),y=Murder))+
   geom_bar(stat = 'identity')+
-  coord_flip()
+  coord_flip() #it creates bar graph between (state and Murder) vs Murder 
 
 # adding color # would murder arrests be related to the percentage of urban population in the state?
 ggplot(data = usa_arrests,aes(x=reorder(state,Murder),y=Murder,fill=UrbanPop))+
@@ -122,27 +122,28 @@ ggplot(data = usa_arrests,aes(x=reorder(state,Murder),y=Murder,fill=UrbanPop))+
 
 # adding size
 ggplot(data = usa_arrests,aes(x=Assault,y=Murder, size=UrbanPop)) +
-  geom_point()
+  geom_point()  ## Scatter plot with point size that represent urban population
+
 
 
 # plotting by south-east and everyone else 
 
-usa_arrests$southeast<-as.numeric(usa_arrests$state%in%c("Florida","Georgia","Mississippi","Lousiana","South Carolina"))
+usa_arrests$southeast<-as.numeric(usa_arrests$state%in%c("Florida","Georgia","Mississippi","Lousiana","South Carolina")) #it creates a new column in usa_arrests that categorize these states "Florida","Georgia","Mississippi","Lousiana","South Carolina" 
 
 
 ggplot(data = usa_arrests,aes(x=Assault,y=Murder, size=UrbanPop, color=southeast)) +
   geom_point()
 
-usa_arrests$southeast<-factor(usa_arrests$southeast,levels = c(1,0),labels = c("South-east state",'other'))
+usa_arrests$southeast<-factor(usa_arrests$southeast,levels = c(1,0),labels = c("South-east state",'other'))  #it categorize the state if 1 it would be south-east state , if 0 it would be other 
 
 ggplot(data = usa_arrests,aes(x=Assault,y=Murder, size=UrbanPop)) +
   geom_point()+
-  facet_wrap(southeast~ .)
+  facet_wrap(southeast~ .)  #it creates a scatter plot that sapreates between south-east and others  
 
 
 ggplot(data = usa_arrests,aes(x=Assault,y=Murder, size=UrbanPop)) +
   geom_point()+
-  facet_grid(southeast ~ .)
+  facet_grid(southeast ~ .) #same as 139 but in a different method 
 
 ## ---- Part 3: Visualizing the spatial data ----
 # Administrative boundaries
